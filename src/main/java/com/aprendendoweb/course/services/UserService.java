eec0fd7a6d4a6947393e6c23ecd4a89a25ce9b6b
Fabiana Aparecida Ribeiro do Vale <fabianavale91@gmail.com>
2025-08-25 15:11:48 -0300

    Dev profile

--- src/main/java/com/aprendendoweb/course/services/UserService.java
+++ src/main/java/com/aprendendoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.aprendendoweb.course.repositories.UserRepository;
 import com.aprendendoweb.course.services.exceptions.DatabaseException;
 import com.aprendendoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
    
@@ -46,11 +48,16 @@ public class UserService {
    }
    
    public User update(Long id, User obj) {
-	   User entity = repository.getReferenceById(id);
-	   updateData(entity, obj);
-	   return repository.save(entity);
-	   
+	   try {
+		   User entity = repository.getReferenceById(id);
+		   updateData(entity, obj);
+		   return repository.save(entity);
+	   }catch(EntityNotFoundException e) {
+		   throw new ResourceNotFoundException(id);
+	   }
+		   
    }
+   
 
    private void updateData(User entity, User obj) {
 	entity.setName(obj.getName());
